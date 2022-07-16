aae684bc1458dea9dcc6c6d74f22eab52c9aa42f
firmends <firmends@yahoo.com>
2022-07-16 12:35:58 -0500

    completed module 4

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -49,6 +49,30 @@ namespace GradeBook.GradeBooks
         }
       }
 
+      public override void CalculateStatistics()
+      {
+        if (this.Students.Count < 5)
+        {
+          Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+        }
+        else
+        {
+          base.CalculateStatistics();
+        }
+      }
+
+      public override void CalculateStudentStatistics()
+      {
+        if (this.Students.Count < 5)
+        {
+          Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+        }
+        else
+        {
+          base.CalculateStudentStatistics();
+        }
+      }
+
       private double GetLowestGrade(IEnumerable<double> grades)
       {
         var lowestGrade = from grade in grades
