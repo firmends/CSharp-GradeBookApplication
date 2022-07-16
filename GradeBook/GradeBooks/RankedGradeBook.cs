fefbd9b7997cf6a729a50bd290bded3104f87221
firmends <firmends@yahoo.com>
2022-07-15 22:32:33 -0500

    First attempt at module 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using GradeBook.Enums;
 
 namespace GradeBook.GradeBooks
@@ -7,6 +10,51 @@ namespace GradeBook.GradeBooks
       public RankedGradeBook(string name) : base(name)
       {
         this.Type = GradeBookType.Ranked;
+      }
+
+      public override char GetLetterGrade(double averageGrade)
+      {
+        if (this.Students.Count < 5)
+        {
+          throw new InvalidOperationException();
+        }
+
+        var gradeThreshold = this.Students.Count / 5;
+        var gradesList = new List<double>();
+
+        foreach (var student in this.Students)
+        {
+          foreach (var grade in student.Grades)
+          {
+            gradesList.Add(grade);
+          }
+        }
+
+        var query = (from grades in gradesList
+                      orderby grades descending
+                      select grades).ToList();
+
+        switch (averageGrade)
+        {
+          case var grade when grade >= GetLowestGrade(query.Take(gradeThreshold)):
+            return 'A';
+          case var grade when grade >= GetLowestGrade(query.Take(gradeThreshold * 2)):
+            return 'B';
+          case var grade when grade >= GetLowestGrade(query.Take(gradeThreshold * 3)):
+            return 'C';
+          case var grade when grade >= GetLowestGrade(query.Take(gradeThreshold * 4)):
+            return 'D';
+          default:
+            return 'F';
+        }
+      }
+
+      private double GetLowestGrade(IEnumerable<double> grades)
+      {
+        var lowestGrade = from grade in grades
+                          orderby grade
+                          select grade;
+        return lowestGrade.Take(1).First();
       }
     }
 }
